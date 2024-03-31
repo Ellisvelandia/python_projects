08182fc7141f2d5522fc72bc478f08f68767c432
Ellis velandia <eyis619@gmail.com>
2024-03-30 22:46:21 -0500

    String that start with a  and end in y

--- demo.py
+++ demo.py
@@ -9,3 +9,22 @@ for number in range(50):
 
 evens = [number for number in range(50) if number % 2 == 0]
 print(evens)        
+
+#String that start with "a" and end in "y"
+
+options = ["any", "albany", "apple","world", "hello", ""]
+valid_strings = []
+
+for string in options:
+    if len(string) <= 1:
+        continue
+    
+    if string[0]  != "a":
+        continue
+    
+    if string[-1] != "y":
+        continue
+    
+    valid_strings.append(string)
+
+print(valid_strings)    
