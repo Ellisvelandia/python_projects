46f23e0300523aaad4b06257983228129035e1aa
Ellis velandia <eyis619@gmail.com>
2024-04-08 16:52:02 -0500

    varible data type

--- variable.py
+++ variable.py
@@ -19,4 +19,45 @@ price = 500
 days = 7
 
 total = (price * days) * 0.7
-print("Special price: ", total)
+# print("Special price: ", total)
+
+# extra = input("Price of add-ons:")
+# extra = int(extra)
+# total = 1000 + extra
+# print(f"Total price ${price}")
+
+# first_name = input("Enter the first name: ")
+# country = input("Enter a country: ")
+# city = input("Enter a city: ")
+
+# print(f"{first_name} lives in {city} {country}")
+
+# rental_price = input("Enter the price per day: ")
+# rental_price = int(rental_price)
+# days = input("Number of days to rent: ")
+# days = int(days)
+
+# total = rental_price * days
+
+# print(f"Total cost {total}")
+
+# package1 = input("Enter the weight of package 1: ")
+# package2 = input("Enter the weight of package 2: ")
+# package3 = input("Enter the weight of package 3: ")
+
+# package1 = int(package1)
+# package2 = int(package2)
+# package3 = int(package3)
+
+# total = (package1 + package2 + package3)
+
+# print(f"Shipment total: {total}")
+
+
+user = input("Enter username: ")
+age = input("Enter your age: ")
+user_id = 786147
+
+user_id = str(user_id)
+
+print(f"Welcome {user} your age is {age} and you userId is {user_id}")
