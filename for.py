84a7baefe681a34d6dbe0250b4f0b49df43b264a
Ellis velandia <eyis619@gmail.com>
2024-09-07 21:40:23 -0500

    for

--- for.py
+++ for.py
@@ -2,14 +2,33 @@
 # for friend in friends:
 #     print(F"hi {friend}")
     
-values = [23,52,59,37,48]
+# values = [23,52,59,37,48]
 
-sum = 0
-length = 0
+# sum = 0
+# length = 0
 
-for value in values:
-    sum += value
-    length += 1    
+# for value in values:
+#     sum += value
+#     length += 1    
     
-print(f"Total sum:{str(sum)} average:{str(sum/length)}")    
-    
+# print(f"Total sum:{str(sum)} average:{str(sum/length)}")    
+
+# product = 1
+# for n in range(1,10):
+#     product = product * n
+
+# print(product)    
+
+for left in range(7):
+    for right in range(left , 7):
+       print("[" + str(left) + "|" + str(right) + "]", end=" ")
+    print()
+
+teams = ["Dragons" , "Wolves" , "Pandas", "Unicorns"]
+
+for home_team in teams:
+    for away_team in teams:
+        if home_team != teams:
+            print(f"{home_team} vs {away_team}")    
+            
+                        
